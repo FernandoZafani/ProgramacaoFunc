e2106ad2c5417731e76e4178f6e8be0a52302874
Fernando Zafani <zafani07@gmail.com>
2022-08-22 14:46:20 -0300

    startexerc

---
+++ exercicios.py
@@ -0,0 +1,16 @@
+# exercicio 1
+print('ex 1')
+
+a = "abcde"
+print(a [::-1])
+
+# exercicio 2
+print('ex 2')
+
+texto = 'Python é uma linguagem excelente'
+print('py' in texto)
+print('ing' in texto)
+print('zzz' in texto)
+
+#exercicio 3
+print('ex 3')
